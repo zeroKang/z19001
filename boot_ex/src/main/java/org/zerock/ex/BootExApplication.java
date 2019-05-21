a1dcf1f08890025955e3f9c052599b54ce0374c2
zzz <zzz@DESKTOP-829ALLD>
2019-05-22 00:39:56 +0900

    first

---
+++ boot_ex/src/main/java/org/zerock/ex/BootExApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootExApplication.class, args);
+	}
+
+}
