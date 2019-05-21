c8485474d2509068ab71a880a2d0b4f3373abd85
zzz <zzz@DESKTOP-829ALLD>
2019-05-22 01:01:29 +0900

    3rd

--- boot_ex/src/main/java/org/zerock/ex/controller/SampleController.java
+++ boot_ex/src/main/java/org/zerock/ex/controller/SampleController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/sample")
 public class SampleController {
@@ -11,6 +14,9 @@ public class SampleController {
 
 	@GetMapping("/hello")
 	public String hello() {
+		
+		log.info("sample.........hello");
+		
 		return "hello";
 	}
 	
